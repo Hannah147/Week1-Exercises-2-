3309be501df9fb735205e15151bcc9a883b9dc3d
Hannah147 <47416196+Hannah147@users.noreply.github.com>
2020-01-25 17:05:53 +0000

    Added ToString to album class

--- Week1-Exercises/Album.cs
+++ Week1-Exercises/Album.cs
@@ -11,5 +11,12 @@ namespace Week1_Exercises
         public string AlbumName { get; set; }
         public DateTime YearReleased { get; set; }
         public int AlbumSales { get; set; }
+
+        public override string ToString()
+        {
+            return $"{AlbumName}"+
+                $"Year Released : {YearReleased}" +
+                $"Sales : {AlbumSales}";
+        }
     }
 }
