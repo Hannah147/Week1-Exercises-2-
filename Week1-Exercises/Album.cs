bc91d307b243205d90956e8b7b03052d83d65369
Hannah147 <47416196+Hannah147@users.noreply.github.com>
2020-01-25 16:42:04 +0000

    Added Album Class and properties

---
+++ Week1-Exercises/Album.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1_Exercises
+{
+    class Album
+    {
+        public string AlbumName { get; set; }
+        public DateTime YearReleased { get; set; }
+        public int AlbumSales { get; set; }
+    }
+}
